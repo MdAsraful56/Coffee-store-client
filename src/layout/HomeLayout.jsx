4d35eefc0dd0bd5594374aa652c6ee7d9e082ff6
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-12 08:39:49 +0600

    title card design complete

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import nameIcon from '../assets/more/15.jpg'
+import capIcon from '../assets/more/logo1.png'
 
 const HomeLayout = () => {
     return (
-        <div>
-            <div className=" " >
-                <h1 className="text-3xl text-center rancho">Espresso Emporium</h1>
+        <div className=''>
+            {/* title card section  */}
+            <div className="relative w-full" >
+                <img src={nameIcon} alt="" className='h-16 w-full object-cover' />
+                <div className="absolute inset-0 flex flex-row items-center justify-center">
+                    <img src={capIcon} alt="" className="w-20 h-20 mb-2 mt-2" />
+                    <h1 className="text-3xl font-semibold rancho">Espresso Emporium</h1>
+                </div>
             </div>
+            {/* header section  */}
+            
         </div>
     );
 };
