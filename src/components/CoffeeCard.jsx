73dfbaa5e8fdcfcb549190a0a8753da313b47577
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-13 08:21:08 +0600

    coffee card data is upgreate price is blanck

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
 const CoffeeCard = ({coffee}) => {
-    console.log(coffee)
+    // console.log(coffee);
+
+    const {name, chef, supplier, taste, category, details, photo} = coffee;
+
     return (
         <div>
-            
+            <div className="">
+                <img src={photo} className='' alt="" />
+            </div>
+            <div className="">
+                <div className="">
+                    <h4 className="">{name}</h4>
+                    <h4 className="">{chef}</h4>
+                    <h4 className="">{supplier}</h4>
+                    <h4 className="">{taste}</h4>
+                </div>
+                <div className="">
+                    <button className="btn">1</button>
+                    <button className="btn">2</button>
+                    <button className="btn">3</button>
+                </div>
+            </div>
         </div>
     );
 };
